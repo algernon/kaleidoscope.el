f13a4dc9341849c61c4a4beaff9493878c9fde70
Gergely Nagy <algernon@madhouse-project.org>
2017-07-27 13:08:05 +0200

    Linter-suggested changes
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- kaleidoscope-evil-state-flash.el
+++ kaleidoscope-evil-state-flash.el
@@ -1,10 +1,11 @@
-;;; kaleidoscope-evil-state-flash.el -- Flash keyboard LEDs when changing Evil state
+;;; kaleidoscope-evil-state-flash.el --- Flash keyboard LEDs when changing Evil state
 ;;
 ;; Copyright (c) 2017 Gergely Nagy
 ;;
 ;; Author: Gergely Nagy
 ;; URL: https://github.com/algernon/kaleidoscope.el
-;; Package-Requires: ((evil "1.2.12"))
+;; Version: 0.1.0
+;; Package-Requires: ((evil "1.2.12") (kaleidoscope "0.1.0"))
 ;;
 ;; This file is NOT part of GNU Emacs.
 ;;
@@ -20,70 +21,72 @@
 
 ;;;; Settings
 
-(defgroup kaleidoscope/evil-state-flash nil
-  "Customization group for 'kaleidoscope/evil-state-flash'."
+(defgroup kaleidoscope-evil-state-flash nil
+  "Customization group for 'kaleidoscope-evil-state-flash'."
   :group 'kaleidoscope)
 
-(defcustom kaleidoscope/evil-state-flash/insert-state-color "#66CE00"
+(defcustom kaleidoscope-evil-state-flash-insert-state-color "#66CE00"
   "Color to flash when entering the 'insert' state."
-  :group 'kaleidoscope/evil-state-flash
+  :group 'kaleidoscope-evil-state-flash
   :type 'color)
 
-(defcustom kaleidoscope/evil-state-flash/visual-state-color "#BFBFBF"
+(defcustom kaleidoscope-evil-state-flash-visual-state-color "#BFBFBF"
   "Color to flash when entering the 'visual' state."
-  :group 'kaleidoscope/evil-state-flash
+  :group 'kaleidoscope-evil-state-flash
   :type 'color)
 
-(defcustom kaleidoscope/evil-state-flash/normal-state-color "#EFAE0E"
+(defcustom kaleidoscope-evil-state-flash-normal-state-color "#EFAE0E"
   "Color to flash when entering the 'normal' state."
-  :group 'kaleidoscope/evil-state-flash
+  :group 'kaleidoscope-evil-state-flash
   :type 'color)
 
-(defcustom kaleidoscope/evil-state-flash/replace-state-color "#D3691E"
+(defcustom kaleidoscope-evil-state-flash-replace-state-color "#D3691E"
   "Color to flash when entering the 'replace' state."
-  :group 'kaleidoscope/evil-state-flash
+  :group 'kaleidoscope-evil-state-flash
   :type 'color)
 
-(defcustom kaleidoscope/evil-state-flash/duration "1 sec"
+(defcustom kaleidoscope-evil-state-flash-duration "1 sec"
   "Duration of the flash."
-  :group 'kaleidoscope/evil-state-flash
+  :group 'kaleidoscope-evil-state-flash
   :type 'string)
 
 ;;;; Helpers
 
-(defun kaleidoscope/evil-state-flash ()
+(defun kaleidoscope-evil-state-flash ()
   "Flash the keyboard with colors depending on 'evil-next-state'."
-  (let ((color (eval (intern (concat "kaleidoscope/evil-state-flash"
+  (let ((color (eval (intern (concat "kaleidoscope-evil-state-flash"
                                      (symbol-name evil-next-state)
                                      "-state-color")))))
-    (kaleidoscope/send-command :led/setAll (kaleidoscope/color-to-rgb color))
-    (run-at-time kaleidoscope/evil-state-flash/duration nil
-                 (kaleidoscope/send-command :led/setAll "0 0 0"))))
+    (kaleidoscope-send-command :led/setAll (kaleidoscope-color-to-rgb color))
+    (run-at-time kaleidoscope-evil-state-flash-duration nil
+                 (kaleidoscope-send-command :led/setAll "0 0 0"))))
 
 ;;;; Main entry point
 
 ;;;###autoload
-(defun kaleidoscope/evil-state-flash-setup ()
+(defun kaleidoscope-evil-state-flash-setup ()
   "Set up hooks to flash the keyboard when changing Evil states."
   (interactive)
 
   (mapc (lambda (state)
-          (add-hook state #'kaleidoscope/evil-state-flash))
+          (add-hook state #'kaleidoscope-evil-state-flash))
         '(evil-insert-state-entry-hook
           evil-visual-state-entry-hook
           evil-replace-state-entry-hook
           evil-normal-state-entry-hook)))
 
 ;;;###autoload
-(defun kaleidoscope/evil-state-flash-teardown ()
-  "Remove the hooks set up by 'kaleidoscope/evil-state-flash-setup'."
+(defun kaleidoscope-evil-state-flash-teardown ()
+  "Remove the hooks set up by 'kaleidoscope-evil-state-flash-setup'."
   (interactive)
 
   (mapc (lambda (state)
-          (remove-hook state #'kaleidoscope/evil-state-flash))
+          (remove-hook state #'kaleidoscope-evil-state-flash))
         '(evil-insert-state-entry-hook
           evil-visual-state-entry-hook
           evil-replace-state-entry-hook
           evil-normal-state-entry-hook)))
 
 (provide 'kaleidoscope-evil-state-flash)
+
+;;; kaleidoscope-evil-state-flash.el ends here
--- kaleidoscope.el
+++ kaleidoscope.el
@@ -1,9 +1,10 @@
-;;; kaleidoscope.el -- Controlling Kaleidoscope-powered devices
+;;; kaleidoscope.el --- Controlling Kaleidoscope-powered devices.
 ;;
 ;; Copyright (c) 2017 Gergely Nagy
 ;;
 ;; Author: Gergely Nagy
 ;; URL: https://github.com/algernon/kaleidoscope.el
+;; Version: 0.1.0
 ;; Package-Requires: ((s "1.11.0"))
 ;;
 ;; This file is NOT part of GNU Emacs.
@@ -26,14 +27,14 @@
   "Customization group for 'kaleidoscope'."
   :group 'comm)
 
-(defcustom kaleidoscope/device-port "/dev/ttyACM0"
+(defcustom kaleidoscope-device-port "/dev/ttyACM0"
   "Serial port the device is connected to."
   :group 'kaleidoscope
   :type 'string)
 
 ;;;; Helpers
 
-(defun kaleidoscope/send-command (command &optional args)
+(defun kaleidoscope-send-command (command &optional args)
   "Send a command with (optional) arguments to the device. The
 command must be a keyword, and the arguments a pre-formatted
 string."
@@ -42,7 +43,7 @@ string."
            (list (s-chop-prefix ":" (s-replace "/" "." (symbol-name command)))
                  args))))
 
-(defun kaleidoscope/color-to-rgb (color)
+(defun kaleidoscope-color-to-rgb (color)
   "Convert a color name or hexadecimal RGB representation to a
 string suitable for Kaleidoscope's LED commands."
   (mapconcat (lambda (c) (format "%d" (round (* 255 c))))
@@ -53,10 +54,10 @@ string suitable for Kaleidoscope's LED commands."
 
 ;;;###autoload
 (defun kaleidoscope-start ()
-  "Connect to the device on 'kaleidoscope/device-port'."
+  "Connect to the device on 'kaleidoscope-device-port'."
   (interactive)
 
-  (make-serial-process :port kaleidoscope/device-port
+  (make-serial-process :port kaleidoscope-device-port
                        :speed 9600
                        :name "*kaleidoscope*"
                        :buffer "*kaleidoscope*"))
@@ -67,3 +68,5 @@ string suitable for Kaleidoscope's LED commands."
   (interactive)
 
   (quit-process "*kaleidoscope*"))
+
+;;; kaleidoscope.el ends here
