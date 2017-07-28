078a959834f0cd84c2cd7b7bf53df44346e49d1a
Gergely Nagy <algernon@madhouse-project.org>
2017-07-28 11:23:37 +0200

    kaleidoscope-evil-state-flash.el: Less needless flashing
    Do not flash the keyboard if the state did not change. Don't flash it either if
    the previous state was not something we handle.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- kaleidoscope-evil-state-flash.el
+++ kaleidoscope-evil-state-flash.el
@@ -54,12 +54,14 @@
 
 (defun kaleidoscope-evil-state-flash ()
   "Flash the keyboard with colors depending on 'evil-next-state'."
-  (let ((color (symbol-value (intern (concat "kaleidoscope-evil-state-flash-"
-                                             (symbol-name evil-next-state)
-                                             "-state-color")))))
-    (kaleidoscope-send-command :led/setAll (kaleidoscope-color-to-rgb color))
-    (run-at-time kaleidoscope-evil-state-flash-duration nil
-                 (lambda () (kaleidoscope-send-command :led/setAll "0 0 0")))))
+  (when (and (not (equal evil-previous-state evil-next-state))
+             (member evil-previous-state '(normal insert visual replace)))
+    (let ((color (symbol-value (intern (concat "kaleidoscope-evil-state-flash-"
+                                              (symbol-name evil-next-state)
+                                              "-state-color")))))
+     (kaleidoscope-send-command :led/setAll (kaleidoscope-color-to-rgb color))
+     (run-at-time kaleidoscope-evil-state-flash-duration nil
+                  (lambda () (kaleidoscope-send-command :led/setAll "0 0 0"))))))
 
 ;;;; Main entry point
 
